998dba9041b2d1f303035a2a6958f9b07588e965
Subhojithara <89927889+Subhojithara@users.noreply.github.com>
2024-12-27 13:28:30 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
